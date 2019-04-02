e82305fc50c27e0439a5e64af1b7dab37dd32b64
Shreyas Sonawane <48873405+Pixotronshrey@users.noreply.github.com>
2019-04-02 20:03:05 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,8 +1,5 @@
 /* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
+/* GitHub : github.com/Pixotronshrey/Neural-network-or-Particles-design-javascript-files
 /* How to use? : Check the GitHub README
 /* v2.0.0
 /* ----------------------------------------------- */
